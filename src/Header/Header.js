aa26fbf361191cd5d3241363e7639ba14d2784ac
solcodeguy <smeighn@gmail.com>
2020-03-12 20:19:35 -0400

    first int

---
+++ src/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css'
+export default function Header() {
+    return ( <
+        header className = "header" >
+        <
+        h1 className = "header_title" > Book - it Up < /h1> <
+        h2 className = "header_subtitle" > Google meets your local bookstore < /h2> < /
+        header >
+    );
+}
